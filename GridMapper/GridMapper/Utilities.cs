2946dcc200a538862ec64c155b8e8fd090b217a6
Ludovic Tresson <tresson@intechinfo.fr>
2012-11-05 12:37:59 +0100

    Setting up the Utilities class for future uses

---
+++ GridMapper/GridMapper/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GridMapper
+{
+	public class Utilities
+	{
+	}
+}
