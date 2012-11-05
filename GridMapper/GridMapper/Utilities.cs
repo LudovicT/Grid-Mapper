e050db609635d8427448f8bbc03f12bde6530263
Ludovic Tresson <tresson@intechinfo.fr>
2012-11-05 13:56:20 +0100

    getMacAddress implementation

--- GridMapper/GridMapper/Utilities.cs
+++ GridMapper/GridMapper/Utilities.cs
@@ -6,10 +6,29 @@ using System.Net.NetworkInformation;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace GridMapper
 {
 	public class Utilities
 	{
+		[DllImport( "iphlpapi.dll", ExactSpelling = true )]
+		public static extern int SendARP( int DestIP, int SrcIP, byte[] pMacAddr, ref uint PhyAddrLen );
+
+		public static String getMacAddress(IPAddress dst)
+		{
+			//IPAddress dst = IPAddress.Parse( "192.168.2.1" );
+			int intAddress = BitConverter.ToInt32( dst.GetAddressBytes(), 0 );
+			byte[] macAddr = new byte[6];
+			uint macAddrLen = (uint)macAddr.Length;
+			if ( SendARP( intAddress, 0, macAddr, ref macAddrLen ) != 0 )
+				throw new InvalidOperationException( "SendARP failed." );
+
+			string[] str = new string[(int)macAddrLen];
+			for ( int i = 0; i < macAddrLen; i++ )
+				str[i] = macAddr[i].ToString( "x2" );
+
+			return string.Join( ":", str );
+		}
 	}
 }
