77e376ef6c2395044953cbf62c8da6ca04783fd6
Ludovic Tresson <tresson@intechinfo.fr>
2012-11-09 11:27:07 +0100

    New Object models implementation

---
+++ GridMapper/GridMapper/NetworkModelObject/INetwork/INetwork.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Collections.ObjectModel;
+
+namespace GridMapper.NetworkModelObject.INetwork
+{
+	interface INetworkItem
+	{
+
+	}
+
+	interface IHub : INetworkItem
+	{
+		ReadOnlyCollection<INetworkItem> NetworkItems{get;}
+	}
+
+	interface ISwitch : INetworkItem
+	{
+		struct CAMdata
+		{
+			int _port;
+			int _VLAN;
+			PhysicalAddress _mac;
+			int _TTL;
+		}
+		HashSet<CAMdata> CAMTable{ get; }
+	}
+
+	interface INetworkInterface : INetworkItem
+	{
+		PhysicalAddress Mac { get; }
+	}
+
+	interface IAdministrableSwitch : ISwitch,INetworkInterface
+	{
+
+	}
+
+	interface IRouter : INetworkInterface
+	{
+
+	}
+	/*interface IEthernetDevice
+	{
+
+	}
+	 * interface IBridge
+	{
+
+	}
+	 * interface IWifiDevice
+	{
+
+	}
+	 * interface IFireWall
+	{
+
+	}
+	 * interface IServer
+	{
+
+	}
+	 * interface Server
+	{
+
+	}
+	 * interface Host
+	{
+
+	}
+	 */
+}
