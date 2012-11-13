b3fb59d9c9d72524f005e752eab0974cb2398c6f
Ludovic Tresson <tresson@intechinfo.fr>
2012-11-13 13:04:14 +0100

    Modified the arguments and the start behavior in console mode.

--- GridMapper/GridMapper/NetworkModelObject/INetwork/INetwork.cs
+++ GridMapper/GridMapper/NetworkModelObject/INetwork/INetwork.cs
@@ -19,14 +19,14 @@ namespace GridMapper.NetworkModelObject.INetwork
 
 	interface ISwitch : INetworkItem
 	{
-		struct CAMdata
-		{
-			int _port;
-			int _VLAN;
-			PhysicalAddress _mac;
-			int _TTL;
-		}
 		HashSet<CAMdata> CAMTable{ get; }
+	}
+	struct CAMdata
+	{
+		int _port;
+		int _VLAN;
+		PhysicalAddress _mac;
+		int _TTL;
 	}
 
 	interface INetworkInterface : INetworkItem
