ec2e831a2da1d1af90e251dd352aa3c499234749
Aztheroth <bidouille_77@hotmail.fr>
2012-11-15 23:19:19 +0100

    fix !

--- GridMapper/GridMapper/NetworkModelObject/NetworkBuilder/AutoBuilder.cs
+++ GridMapper/GridMapper/NetworkModelObject/NetworkBuilder/AutoBuilder.cs
@@ -12,28 +12,28 @@ namespace GridMapper.NetworkModelObject
 		
 		public AutoBuilder()
 		{
-			if(_network == null)
-				_network = new Network();
+			//if(_network == null)
+			//    _network = new Network();
 		}
 
 		public void SuperPingerHandling( PingReply pingReply )
 		{
-			//test if exist interface with this ip
-			NetworkInterface networkInterface = new NetworkInterface(pingReply.Address, pingReply.RoundtripTime);
-			Host host = new Host( networkInterface );
-			_network.NetworkHost.GetOrAdd(pingReply.Address, host );
+			////test if exist interface with this ip
+			//NetworkInterface networkInterface = new NetworkInterface(pingReply.Address, pingReply.RoundtripTime);
+			//Host host = new Host( networkInterface );
+			//_network.NetworkHost.GetOrAdd(pingReply.Address, host );
 		}
 
 		public void MacAddressHandling( IPAddress ipAddress, PhysicalAddress macAddress )
 		{
-			Host networkHost;
-			if( _network.NetworkHost.TryGetValue( ipAddress, out networkHost ) )
-			{
-				INetworkInterface networkInterface;
-				if( networkHost.NetworkInterfaces.TryGetValue( ipAddress, out networkInterface ) )
-					networkInterface.Mac = macAddress;
-			}
-			Console.WriteLine( ipAddress.ToString() + macAddress.ToString() );
+			//Host networkHost;
+			//if( _network.NetworkHost.TryGetValue( ipAddress, out networkHost ) )
+			//{
+			//    INetworkInterface networkInterface;
+			//    if( networkHost.NetworkInterfaces.TryGetValue( ipAddress, out networkInterface ) )
+			//        networkInterface.Mac = macAddress;
+			//}
+			//Console.WriteLine( ipAddress.ToString() + macAddress.ToString() );
 		}
 	}
 }
