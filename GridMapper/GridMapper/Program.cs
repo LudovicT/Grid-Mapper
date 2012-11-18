5be9ece095f707fb96d5f3f69da07b1852a4df68
Ludovic Tresson <tresson@intechinfo.fr>
2012-11-18 20:38:12 +0100

    Extracted startup and created Execution

--- GridMapper/GridMapper/Program.cs
+++ GridMapper/GridMapper/Program.cs
@@ -137,54 +137,5 @@ namespace GridMapper
 			Console.WriteLine( "	-t,-tasks	Specify the maximum simultanous task running (default is 50)" );
 		}
 
-		class Startup
-		{
-			bool _cmdConsole;
-			int _maximumTasks;
-			List<IPAddress> _ipToTest;
-
-			public Startup()
-			{
-				_cmdConsole = false;
-				_maximumTasks = 50;
-				//_ipToTest = ;
-			}
-
-			public bool CmdConsole
-			{
-				get
-				{
-					return _cmdConsole;
-				}
-				set
-				{
-					_cmdConsole = value;
-				}
-			}
-
-			public int MaximumTasks
-			{
-				get
-				{
-					return _maximumTasks;
-				}
-				set
-				{
-					_maximumTasks = value;
-				}
-			}
-
-			public List<IPAddress> IpToTest
-			{
-				get
-				{
-					return _ipToTest;
-				}
-				set
-				{
-					_ipToTest = value;
-				}
-			}
-		}
 	}
 }
