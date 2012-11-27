4e4f329ae34e178c7fc37b709268582e245b45f5
Ludovic Tresson <tresson@intechinfo.fr>
2012-11-27 10:40:37 +0100

    add the start of the taskmanager

---
+++ GridMapper/GridMapper/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridMapper
+{
+	public class TaskManager
+	{
+		public object run(Func<object> action)
+		{
+			Task<object> task = Task.Factory.StartNew<object>( action );
+			return task.Result;
+		}
+	}
+}
