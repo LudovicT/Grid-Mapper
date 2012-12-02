0c63bdac4051ccf7a61a42cb0297cf75aa726e0a
Ludovic Tresson <tresson@intechinfo.fr>
2012-12-02 19:56:26 +0100

    Changed the IPParser

--- GridMapper/GridMapper/Parameters/IPsParser.cs
+++ GridMapper/GridMapper/Parameters/IPsParser.cs
@@ -6,52 +6,161 @@ using System.Net;
 
 namespace GridMapper.Parameters
 {
-	class IPsParser
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Net;
+	using System.Diagnostics;
+
+	namespace IPParser
 	{
-		public List<IPAddress> IPArgumentsParser(string IPToParse)
+		public class IPParserResult
 		{
-			List<IPAddress> ListIPs = new List<IPAddress>();
-			Console.WriteLine( "IPsToParse = \'" + IPToParse + "\'" );
-			string[] BigBlocksIP = IPToParse.Split( ',' );
-			if ( BigBlocksIP.Length > 1 )
+			internal IPParserResult( string errorMessage, IEnumerable<IPAddressRange> result )
 			{
-				for ( int iterator = 0; iterator < BigBlocksIP.Length; iterator++ )
+				Debug.Assert( ( errorMessage == null ) == ( result != null ) );
+				ErrorMessage = errorMessage;
+				Result = result;
+			}
+
+			public bool HasError { get { return ErrorMessage != null; } }
+			public string ErrorMessage { get; private set; }
+			public IEnumerable<IPAddressRange> Result { get; private set; }
+		}
+
+		public class IPAddressRange
+		{
+			public IPAddress From { get; private set; }
+			public IPAddress To { get; private set; }
+
+		}
+
+		public static class IParser
+		{
+			static public IPParserResult TryParse( string rangeOfIP )
+			{
+				Parser parser = new Parser(rangeOfIP);
+				return null;
+			}
+
+			class Parser
+			{
+				readonly string _s;
+				int _pos;
+
+
+				public Parser( string s )
 				{
-					string[] SmallBlocksIP = BigBlocksIP[ iterator ].Split( '-' );
-					if ( SmallBlocksIP.Length > 1 )
+					_s = s;
+				}
+
+				char Current { get { return _s[_pos]; } }
+
+				bool Forward()
+				{
+					// Skip whitespaces...
+					return ++_pos < _s.Length;
+				}
+
+				bool IsEndOfInput { get { return _pos >= _s.Length; } }
+
+				public bool IsIPAddressRange( out IPAddressRange r )
+				{
+					if ( MatchChar( '-' ) )
 					{
-						for ( int iterator2 = 0; iterator2 < SmallBlocksIP.Length; iterator2++ )
+						
+					}
+					r = null;
+					return false;
+				}
+
+				public bool IsIPAddressFull( out IPAddress ip )
+				{
+					ip = null;
+					byte b1;
+					byte b2;
+					byte b3;
+					byte b4;
+					if ( IsByte( out b1 )
+						&& MatchChar( '.' )
+						&& IsByte( out b2 )
+						&& MatchChar( '.' )
+						&& IsByte( out b3 )
+						&& MatchChar( '.' )
+						&& IsByte( out b4 ) )
+					{
+						ip = new IPAddress( new byte[] { b1, b2, b3, b4 } );
+						return true;
+					}
+					return false;
+				}
+
+				public bool IsIPAddressShort( out IPAddress ip, byte autoFillValue = 0 )
+				{
+					ip = null;
+					byte b1;
+					byte b2 = autoFillValue;
+					byte b3 = autoFillValue;
+					byte b4 = autoFillValue;
+					if ( IsByte( out b1 ) )
+					{
+						if ( MatchChar( '.' ) && IsByte( out b2 ) )
 						{
-							if ( ( iterator2 + 1 ) < SmallBlocksIP.Length )
+							if ( MatchChar( '.' ) && IsByte( out b3 ) )
 							{
-								IPAddress StartingIP = IPAddress.Parse( SmallBlocksIP[ iterator2 ] );
-								IPAddress EndingIP = IPAddress.Parse( SmallBlocksIP[ iterator2 + 1 ] );
-								Console.WriteLine("SIP " + StartingIP);
-								Console.WriteLine("EIP " + EndingIP);
-								ListIPs = IPRangeGenerator( ListIPs, StartingIP, EndingIP );
-								continue;
+								if ( MatchChar( '.' ) && IsByte( out b4 ) )
+								{
+								}
+							}
+						}
+						ip = new IPAddress( new byte[] { b1, b2, b3, b4 } );
+						return true;
+					}
+					return false;
+				}
+
+				private bool MatchChar( char c )
+				{
+					if ( !IsEndOfInput && Current == c )
+					{
+						Forward();
+						return true;
+					}
+					return false;
+				}
+
+				private bool IsByte( out byte b )
+				{
+					b = 0;
+					string bString = "";
+					if ( Char.IsDigit( Current ) )
+					{
+						bString += Current;
+						Forward();
+						if ( Char.IsDigit( Current ) )
+						{
+							bString += Current;
+							Forward();
+							if ( Char.IsDigit( Current ) )
+							{
+								bString += Current;
 							}
 						}
 					}
-					else
+					if( bString != String.Empty)
 					{
-						ListIPs.Add( IPAddress.Parse( BigBlocksIP[ iterator ] ) ) ;
+						if ( byte.TryParse( bString, out b ) )
+						{
+							return true;
+						}
+						return false;
 					}
+					return false;
 				}
+
 			}
-			else
-			{
-				ListIPs.Add( IPAddress.Parse( IPToParse ) );
-			}
-			return ListIPs;
-		}
-		private List<IPAddress> IPRangeGenerator( List<IPAddress> ListIPs, IPAddress StartingIP, IPAddress EndingIP )
-		{
-			for ( uint IPToGenerate = IPRange.IPAddressToUint( StartingIP ); IPToGenerate <= IPRange.IPAddressToUint( EndingIP ); IPToGenerate++ )
-			{
-				ListIPs.Add( IPAddress.Parse( IPToGenerate.ToString() ));
-			}
-			return ListIPs;
+
 		}
 	}
 }
