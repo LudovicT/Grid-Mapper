d01d817b3370df00ff2428a7ed22d6da69adc8d5
Aztheroth <bidouille_77@hotmail.fr>
2012-12-03 11:10:29 +0100

    xmlwriter

---
+++ GridMapper/TestGridMapper/TestRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using GridMapper.NetworkRepository;
+using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GridMapper.Test
+{
+	[TestFixture]
+	public class TestRepository
+	{
+		[Test]
+		public void TestXmlWriter()
+		{
+			Repository repo = new Repository();
+			PingSender pingSender = new PingSender();
+			ARPSender arpSender = new ARPSender();
+			Task task1 = Task.Factory.StartNew( () =>
+			{
+				for( int i = 0 ; i < 10 ; i++ )
+				{
+					Task task2 = Task.Factory.StartNew( () =>
+					{
+						repo.AddOrUpdate( IPAddress.Parse( "10.8.99.121" ), pingSender.Ping( IPAddress.Parse( "10.8.99.121" ) ) );
+						repo.AddOrUpdate( IPAddress.Parse( "10.8.99.121" ), arpSender.GetMac( IPAddress.Parse( "10.8.99.121" ) ) );
+					} );
+				}
+				Thread.Sleep( 5000 );
+			} ).ContinueWith( ( a ) =>
+			{
+				//pour le moment voir direct dans le dossier
+				repo.XmlWriter();
+			} );
+		}
+
+	}
+}
