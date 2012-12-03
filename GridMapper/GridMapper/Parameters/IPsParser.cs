0e8216680854329b2e000da6c23a0eb94cfb53fe
Ludovic Tresson <tresson@intechinfo.fr>
2012-12-03 15:29:36 +0100

    Update of IPParser

--- GridMapper/GridMapper/Parameters/IPsParser.cs
+++ GridMapper/GridMapper/Parameters/IPsParser.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace GridMapper
 {
@@ -12,7 +13,7 @@ namespace GridMapper
 		public IPParserResult()
 		{
 		}
-		internal IPParserResult( string errorMessage, IEnumerable<IPAddressRange> result )
+		internal IPParserResult( string errorMessage, IEnumerable<IPAddressV4Range> result )
 		{
 			Debug.Assert( ( errorMessage == null ) == ( result != null ) );
 			ErrorMessage = errorMessage;
@@ -21,15 +22,82 @@ namespace GridMapper
 
 		public bool HasError { get { return ErrorMessage != null; } }
 		public string ErrorMessage { get; private set; }
-		public IEnumerable<IPAddressRange> Result { get; private set; }
+		public IEnumerable<IPAddressV4Range> Result { get; private set; }
 	}
 
-	public class IPAddressRange
+	[StructLayout( LayoutKind.Explicit )]
+	public struct IPAddressV4
 	{
-		public IPAddress From { get; private set; }
-		public IPAddress To { get; private set; }
+		[FieldOffset( 0 )]
+		public byte B0;
+		[FieldOffset( 1 )]
+		public byte B1;
+		[FieldOffset( 2 )]
+		public byte B2;
+		[FieldOffset( 3 )]
+		public byte B3;
+		[FieldOffset( 0 )]
+		public int Address;
+
+		public override string ToString()
+		{
+			return String.Format( "{0}.{1}.{2}.{3}", B0, B1, B2, B3 );
+		}
+
+		public override bool Equals( object obj )
+		{
+			if ( obj is IPAddressV4 )
+			{
+				return Address == ( (IPAddressV4)obj ).Address;
+			}
+			if ( obj is int )
+			{
+				return Address == (int)obj;
+			}
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return Address;
+		}
+	}
+
+	public enum Token
+	{
+		Unknow,
+		New,
+		Range,
+		CIDR,
+		End,
+	}
+
+	public class IPRangeList : IEnumerable<IPAddressV4>
+	{
+		public void Add( IPAddressV4 ip )
+		{
+		}
+
+		public void Add( IPAddressV4 from, IPAddressV4 to )
+		{
+		}
+
+		public void Remove( IPAddressV4 ip )
+		{
+		}
+
+		public void Remove( IPAddressV4 from, IPAddressV4 to )
+		{
+		}
+
+	}
+
+	public class IPAddressV4Range
+	{
+		public IPAddressV4 From { get; private set; }
+		public IPAddressV4 To { get; private set; }
 			
-		public IPAddressRange( IPAddress ipFrom, IPAddress ipTo )
+		public IPAddressV4Range( IPAddressV4 ipFrom, IPAddressV4 ipTo )
 		{
 			From = ipFrom;
 			To = ipTo;
@@ -42,29 +110,25 @@ namespace GridMapper
 		static public IPParserResult TryParse( string rangeOfIP )
 		{
 			Parser parser = new Parser(rangeOfIP);
-			List<IPAddressRange> Result = new List<IPAddressRange>();
-			IPAddress CurrentIP;
-			IPAddressRange IPRange;
+			List<IPAddressV4Range> Result = new List<IPAddressV4Range>();
+			IPAddressV4 CurrentIP;
+			IPAddressV4 IPRange;
 			string errorMessage = null;
 
-			while ( parser.IsIPAddressShort( out CurrentIP ) )
+			while ( parser.IsIPAddressV4Short( out CurrentIP ) )
 			{
-				if(parser.IsNewIPAddress)
+				if ( parser.NextToken() == Token.New )
 				{
-					Result.Add( new IPAddressRange( CurrentIP, CurrentIP ) );
+					Result.Add( new IPAddressV4Range( CurrentIP, CurrentIP ) );
 				}
-				else if ( parser.IsIPAddressRange( CurrentIP, out IPRange ) )
+				if ( parser.NextToken() == Token.Range )
 				{
-					Result.Add( IPRange );
-					if ( parser.IsNewIPAddress )
-					{
-						continue;
-					}
-					break;
+					parser.IsIPAddressV4Short( out IPRange , 255);
+					Result.Add( new IPAddressV4Range( CurrentIP, IPRange ) );
 				}
 				else
 				{
-					Result.Add( new IPAddressRange( CurrentIP, CurrentIP ) );
+					Result.Add( new IPAddressV4Range( CurrentIP, CurrentIP ) );
 				}
 			}
 			return new IPParserResult(errorMessage, Result);
@@ -83,26 +147,53 @@ namespace GridMapper
 
 			char Current { get { return _s[_pos]; } }
 
-			char Next { get { return _s[_pos+1]; } }
-
 			bool Forward()
 			{
 				// Skip whitespaces...
-				while ( Current == ' ' && Next == ' ') { _pos++; }
-				return ++_pos < _s.Length;
+				if ( Current == ' ' )
+				{
+					while ( !IsEndOfInput && Current == ' ' ) _pos++;
+					return true;
+				}
+				else
+				{
+					return ++_pos < _s.Length;
+				}
 			}
 
 			bool IsEndOfInput { get { return _pos >= _s.Length; } }
 
-			public bool IsIPAddressRange(IPAddress ipFrom, out IPAddressRange r )
+			public Token NextToken()
+			{
+				if ( !IsEndOfInput )
+				{
+					switch ( Current )
+					{
+						case ',':
+							Forward();
+							return Token.New;
+						case '-':
+							Forward();
+							return Token.Range;
+						case '/':
+							Forward();
+							return Token.CIDR;
+						default:
+							return Token.Unknow;
+					}
+				}
+				return Token.End;
+			}
+
+			public bool IPAddressV4Range(IPAddressV4 ipFrom, out IPAddressV4Range r )
 			{
 				r = null;
 				if ( MatchChar( '-' ) )
 				{
-					IPAddress ipTo = null;
-					if ( IsIPAddressShort( out ipTo, 255 ) )
+					IPAddressV4 ipTo;
+					if ( IsIPAddressV4Short( out ipTo, 255 ) )
 					{
-						r = new IPAddressRange( ipFrom, ipTo );
+						r = new IPAddressV4Range( ipFrom, ipTo );
 						return true;
 					}
 					return false;
@@ -110,11 +201,11 @@ namespace GridMapper
 				return false;
 			}
 
-			public bool IsNewIPAddress { get { return MatchChar( ',' );}}
+			public bool IsNewIPAddressV4 { get { return MatchChar( ',' );}}
 
-			public bool IsIPAddressFull( out IPAddress ip )
+			public bool IsIPAddressV4Full( out IPAddressV4 ip )
 			{
-				ip = null;
+				ip = new IPAddressV4();
 				byte b1;
 				byte b2;
 				byte b3;
@@ -127,15 +218,18 @@ namespace GridMapper
 					&& MatchChar( '.' )
 					&& IsByte( out b4 ) )
 				{
-					ip = new IPAddress( new byte[] { b1, b2, b3, b4 } );
+					ip.B0 = b1;
+					ip.B1 = b2;
+					ip.B2 = b3;
+					ip.B3 = b4;
 					return true;
 				}
 				return false;
 			}
 
-			public bool IsIPAddressShort( out IPAddress ip, byte autoFillValue = 0 )
+			public bool IsIPAddressV4Short( out IPAddressV4 ip, byte autoFillValue = 0 )
 			{
-				ip = null;
+				ip = new IPAddressV4();
 				byte b1;
 				byte b2 = autoFillValue;
 				byte b3 = autoFillValue;
@@ -151,7 +245,10 @@ namespace GridMapper
 							}
 						}
 					}
-					ip = new IPAddress( new byte[] { b1, b2, b3, b4 } );
+					ip.B0 = b1;
+					ip.B1 = b2;
+					ip.B2 = b3;
+					ip.B3 = b4;
 					return true;
 				}
 				return false;
