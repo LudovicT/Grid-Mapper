c4c5d2e8dbc7330dde600d10602431b9fb14d44e
aztheroth <bidouille_77@hotmail.fr>
2012-12-28 16:20:27 +0100

    fix

--- GridMapper/GridMapper/NetworkUtilities/PacketReceiver.cs
+++ GridMapper/GridMapper/NetworkUtilities/PacketReceiver.cs
@@ -2,10 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.Ethernet;
+using PcapDotNet.Packets.Arp;
 
 namespace GridMapper.NetworkUtilities
 {
-	class PacketReceiver
+	public class PacketReceiver
 	{
+
+		PacketDevice selectedDevice;
+
+		public PacketReceiver()
+		{
+			IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+			if ( allDevices.Count == 0 )
+			{
+				Console.WriteLine( "No interfaces found! Make sure WinPcap is installed." );
+				return;
+			}
+			selectedDevice = allDevices[0];
+		}
+
+		public void StartReceive()
+		{
+			using( PacketCommunicator communicator = selectedDevice.Open( 42, PacketDeviceOpenAttributes.MaximumResponsiveness, 500 ) )
+			{
+				// Retrieve the packets
+				int nbPackets;
+				//communicator.ReceivePackets(0, PacketHandler );
+
+				Packet packet;
+				do
+				{
+					PacketCommunicatorReceiveResult result = communicator.ReceivePacket( out packet );
+					if( packet != null && packet.Ethernet.IpV4 != null && packet.Ethernet.IpV4.Tcp != null && packet.Ethernet.IpV4.Tcp.IsAcknowledgment && packet.Ethernet.IpV4.Tcp.IsSynchronize && packet.Ethernet.IpV4.Tcp.DestinationPort == 6666 )
+					{
+						switch( result )
+						{
+							case PacketCommunicatorReceiveResult.Timeout:
+								// Timeout elapsed
+								continue;
+							case PacketCommunicatorReceiveResult.Ok:
+								Console.Write( "ip = " + packet.Ethernet.IpV4.Source.ToString() + " Port = " + packet.Ethernet.IpV4.Tcp.SourcePort.ToString() + " portDest : " + packet.Ethernet.IpV4.Tcp.DestinationPort.ToString() );
+								Console.WriteLine();
+								break;
+							default:
+								throw new InvalidOperationException( "The result " + result + " should never be reached here" );
+						}
+					}
+					if( packet != null && packet.Ethernet.EtherType == EthernetType.Arp && packet.Ethernet.Arp.Operation == ArpOperation.Reply )
+					{
+						switch( result )
+						{
+							case PacketCommunicatorReceiveResult.Timeout:
+								// Timeout elapsed
+								continue;
+							case PacketCommunicatorReceiveResult.Ok:
+								Console.Write( "ip = " + packet.Ethernet.Arp.SenderProtocolIpV4Address.ToString() + " mac = " + ByteArrayToHexViaByteManipulation( packet.Ethernet.Arp.SenderHardwareAddress.ToArray() ) );
+								Console.WriteLine();
+								break;
+							default:
+								throw new InvalidOperationException( "The result " + result + " should never be reached here" );
+						}
+					}
+				} while( true );
+			}
+		}
+
+		private string ByteArrayToHexViaByteManipulation( byte[] bytes )
+		{
+			char[] c = new char[bytes.Length * 2];
+			byte b;
+			for( int i = 0 ; i < bytes.Length ; i++ )
+			{
+				b = ((byte)(bytes[i] >> 4));
+				c[i * 2] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+				b = ((byte)(bytes[i] & 0xF));
+				c[i * 2 + 1] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+			}
+			return new string( c );
+		}
 	}
 }
--- GridMapper/GridMapper/NetworkUtilities/PacketSender.cs
+++ GridMapper/GridMapper/NetworkUtilities/PacketSender.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
 
 namespace GridMapper.NetworkUtilities
 {
-	class PacketSender
+	public class PacketSender
 	{
+
+		PacketDevice selectedDevice;
+		PacketCommunicator outputCommunicator;
+
+		public PacketSender()
+		{
+			IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+			if ( allDevices.Count == 0 )
+			{
+				Console.WriteLine( "No interfaces found! Make sure WinPcap is installed." );
+				return;
+			}
+			selectedDevice = allDevices[0];
+
+			outputCommunicator = selectedDevice.Open( 100, PacketDeviceOpenAttributes.Promiscuous, 1000 );
+		}
+
+		public void tryScanPort( Packet packetToSend )
+		{
+			outputCommunicator.SendPacket( packetToSend );
+		}
 	}
 }
