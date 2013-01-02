dd7123934918c2fb1a4b721ef18c4788f9e7c6b7
Ludovic Tresson <tresson@intechinfo.fr>
2013-01-02 13:31:59 +0100

    cidr input modified

--- GridMapper/GridMapper/CIDRUserControl.Designer.cs
+++ GridMapper/GridMapper/CIDRUserControl.Designer.cs
@@ -28,32 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.ipAddressControl1 = new IPAddressControlLib.IPAddressControl();
-			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+			this.ipAddress = new IPAddressControlLib.IPAddressControl();
 			this.slash = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			( (System.ComponentModel.ISupportInitialize)( this.numericUpDown1 ) ).BeginInit();
 			this.SuspendLayout();
 			// 
-			// ipAddressControl1
+			// ipAddress
 			// 
-			this.ipAddressControl1.AllowInternalTab = false;
-			this.ipAddressControl1.AutoHeight = true;
-			this.ipAddressControl1.BackColor = System.Drawing.SystemColors.Window;
-			this.ipAddressControl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.ipAddressControl1.Cursor = System.Windows.Forms.Cursors.IBeam;
-			this.ipAddressControl1.Location = new System.Drawing.Point( 35, 3 );
-			this.ipAddressControl1.MinimumSize = new System.Drawing.Size( 87, 20 );
-			this.ipAddressControl1.Name = "ipAddressControl1";
-			this.ipAddressControl1.ReadOnly = false;
-			this.ipAddressControl1.Size = new System.Drawing.Size( 87, 20 );
-			this.ipAddressControl1.TabIndex = 0;
-			this.ipAddressControl1.Text = "...";
-			// 
-			// maskedTextBox1
-			// 
-			this.maskedTextBox1.Location = new System.Drawing.Point( 142, 3 );
-			this.maskedTextBox1.Name = "maskedTextBox1";
-			this.maskedTextBox1.Size = new System.Drawing.Size( 26, 20 );
-			this.maskedTextBox1.TabIndex = 1;
+			this.ipAddress.AllowInternalTab = false;
+			this.ipAddress.AutoHeight = true;
+			this.ipAddress.BackColor = System.Drawing.SystemColors.Window;
+			this.ipAddress.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.ipAddress.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.ipAddress.Location = new System.Drawing.Point( 35, 3 );
+			this.ipAddress.MinimumSize = new System.Drawing.Size( 87, 20 );
+			this.ipAddress.Name = "ipAddress";
+			this.ipAddress.ReadOnly = false;
+			this.ipAddress.Size = new System.Drawing.Size( 87, 20 );
+			this.ipAddress.TabIndex = 0;
+			this.ipAddress.Text = "...";
 			// 
 			// slash
 			// 
@@ -64,17 +58,36 @@
 			this.slash.TabIndex = 2;
 			this.slash.Text = "/";
 			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point( 142, 3 );
+			this.numericUpDown1.Maximum = new decimal( new int[] {
+            32,
+            0,
+            0,
+            0} );
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size( 36, 20 );
+			this.numericUpDown1.TabIndex = 3;
+			this.numericUpDown1.Value = new decimal( new int[] {
+            24,
+            0,
+            0,
+            0} );
+			this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler( this.numericUpDown1_KeyPress );
+			// 
 			// CIDRUserControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.Window;
+			this.Controls.Add( this.numericUpDown1 );
 			this.Controls.Add( this.slash );
-			this.Controls.Add( this.maskedTextBox1 );
-			this.Controls.Add( this.ipAddressControl1 );
+			this.Controls.Add( this.ipAddress );
 			this.Margin = new System.Windows.Forms.Padding( 2 );
 			this.Name = "CIDRUserControl";
 			this.Size = new System.Drawing.Size( 270, 25 );
+			( (System.ComponentModel.ISupportInitialize)( this.numericUpDown1 ) ).EndInit();
 			this.ResumeLayout( false );
 			this.PerformLayout();
 
@@ -82,9 +95,9 @@
 
         #endregion
 
-		private IPAddressControlLib.IPAddressControl ipAddressControl1;
-		private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+		private IPAddressControlLib.IPAddressControl ipAddress;
 		private System.Windows.Forms.Label slash;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
 
     }
 }
--- GridMapper/GridMapper/CIDRUserControl.cs
+++ GridMapper/GridMapper/CIDRUserControl.cs
@@ -15,5 +15,15 @@ namespace GridMapper
         {
             InitializeComponent();
         }
+
+		private void numericUpDown1_KeyPress( object sender, KeyPressEventArgs e )
+		{
+			if ( !char.IsControl( e.KeyChar )
+				&& !char.IsDigit( e.KeyChar )
+				&& e.KeyChar != '.' )
+			{
+				e.Handled = true;
+			}
+		}
     }
 }
