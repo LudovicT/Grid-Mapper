74794a3deae825bbaef107e6f72c5c2769895335
corozyf <bourreaujules@gmail.com>
2013-01-22 09:48:17 +0100

    I got balls of steel, but not infinite...

--- GridMapper/GridMapper/DiagramDisplayControl.cs
+++ GridMapper/GridMapper/DiagramDisplayControl.cs
@@ -126,14 +126,15 @@ namespace GridMapper
 				//List<IPAddressV4> HostDone = new List<IPAddressV4>();
 
 				
-				foreach (INetworkDictionaryItem item2 in e.ReadOnlyRepository) 
-				{
+				//foreach (INetworkDictionaryItem item2 in e.ReadOnlyRepository) 
+				//{
+
+					DrawEngine(hostNameString, x, y, shapeDict);
 					x += 100;
 					y += 100;
 
-					DrawEngine(hostNameString, x, y, shapeDict);
 					//HostDone.Add(ip);
-				}
+				//}
 			}
 		}
 		#endregion
@@ -150,14 +151,18 @@ namespace GridMapper
 		public void DrawEngine(string s, int x, int y, Dictionary<String, CaptionedShapeBase> dShape)
 		{
 			CircleBase shape = (CircleBase)_NShapeProject.ShapeTypes["Circle"].CreateInstance();
-			shape.Diameter = 150;
+			shape.Diameter = 130;
 			shape.X = x;
 			shape.Y = y;
 			shape.SetCaptionText(0, s);
 
-			_NShapeDiagram.Shapes.Add(shape);
 			if (!dShape.ContainsKey(s))
-				dShape.Add(s, shape);
+			{
+				_NShapeDiagram.Shapes.Add(shape);
+				if (!dShape.ContainsKey(s))
+					dShape.Add(s, shape);
+			}
+
 		}
 	}
 }
