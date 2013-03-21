310e629a49482c8a5c294d9aca2500a846440594
aztheroth <bontemps@intechinfo.fr>
2013-03-21 15:41:05 +0100

    minor change

--- GridMapper/GridMapper/Struct/MacAddress.cs
+++ GridMapper/GridMapper/Struct/MacAddress.cs
@@ -6,45 +6,45 @@ using System.Text;
 
 namespace GridMapper.Struct
 {
-	//[StructLayout( LayoutKind.Explicit )]
-	//public struct MacAddress
-	//{
-	//	[FieldOffset( 0 )]
-	//	public byte B0;
-	//	[FieldOffset( 1 )]
-	//	public byte B1;
-	//	[FieldOffset( 2 )]
-	//	public byte B2;
-	//	[FieldOffset( 3 )]
-	//	public byte B3;
-	//	[FieldOffset( 4 )]
-	//	public byte B4;
-	//	[FieldOffset( 5 )]
-	//	public byte B5;
-	//	[FieldOffset( 0 )]
-	//	public int macAddress;
+	[StructLayout( LayoutKind.Explicit )]
+	public struct MacAddress
+	{
+		[FieldOffset( 0 )]
+		public byte B0;
+		[FieldOffset( 1 )]
+		public byte B1;
+		[FieldOffset( 2 )]
+		public byte B2;
+		[FieldOffset( 3 )]
+		public byte B3;
+		[FieldOffset( 4 )]
+		public byte B4;
+		[FieldOffset( 5 )]
+		public byte B5;
+		[FieldOffset( 0 )]
+		public int MACAddress;
 
-	//	public override string ToString()
-	//	{
-	//		return String.Format( "{X:0}:{X:1}:{X:2}:{X:3}", B0, B1, B2, B3 );
-	//	}
+		public override string ToString()
+		{
+			return String.Format( "{0:X}:{1:X}:{2:X}:{3:X}", B0, B1, B2, B3 );
+		}
 
-	//	public override bool Equals( object obj )
-	//	{
-	//		if (obj is IPAddressV4)
-	//		{
-	//			return Address == ( (IPAddressV4)obj ).Address;
-	//		}
-	//		if (obj is int)
-	//		{
-	//			return Address == (int)obj;
-	//		}
-	//		return false;
-	//	}
+		public override bool Equals( object obj )
+		{
+			if (obj is IPAddressV4)
+			{
+				return MACAddress == ( (IPAddressV4)obj ).MACAddress;
+			}
+			if (obj is int)
+			{
+				return MACAddress == (int)obj;
+			}
+			return false;
+		}
 
-	//	public override int GetHashCode()
-	//	{
-	//		return Address;
-	//	}
-	//}
+		public override int GetHashCode()
+		{
+			return MACAddress;
+		}
+	}
 }
